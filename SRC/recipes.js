8895f230b7492bce074e6cbe13576482e8574a65
pnb3431 <pete.bailen@gmail.com>
2016-06-20 03:02:05 +0000

    working app, recipes being added

---
+++ SRC/recipes.js
@@ -0,0 +1,77 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+module.exports = {
+    "chicken breast":"boneless, skinless chicken breast, should be cooked 8 to 12 minutes over direct medium heat.",
+    "chicken thighs":"boneless, skinless chicken thighs, should be cooked 8 to 10 minutes over direct medium heat.",
+    "steak":"a three quarter inch thick steak, should be cooked for 4 to 6 minutes over direct high heat.  A one inch thick steak, should be cooked 6 to 8 minutes over direct high heat.  A two inch thick steak should be cooked 14 to 18 minutes, with 6 to 8 minutes over direct high heat, and then 8 to 10 minutes over indirect high heat.",
+    "new york strip":"a three quarter inch thick new your strip, should be cooked for 4 to 6 minutes over direct high heat.  A one inch thick new your strip, should be cooked 6 to 8 minutes over direct high heat.  A two inch thick new your strip should be cooked 14 to 18 minutes, with 6 to 8 minutes over direct high heat, and then 8 to 10 minutes over indirect high heat.",
+    "porterhouse":"a three quarter inch thick porterhouse, should be cooked for 4 to 6 minutes over direct high heat.  A one inch thick porterhouse, should be cooked 6 to 8 minutes over direct high heat.  A two inch thick porterhouse should be cooked 14 to 18 minutes, with 6 to 8 minutes over direct high heat, and then 8 to 10 minutes over indirect high heat.",
+    "rib-eye":"a three quarter inch thick rib-eye, should be cooked for 4 to 6 minutes over direct high heat.  A one inch thick rib-eye, should be cooked 6 to 8 minutes over direct high heat.  A two inch thick rib-eye should be cooked 14 to 18 minutes, with 6 to 8 minutes over direct high heat, and then 8 to 10 minutes over indirect high heat.",
+    "t-bone":"a three quarter inch thick t-bone, should be cooked for 4 to 6 minutes over direct high heat.  A one inch thick t-bone, should be cooked 6 to 8 minutes over direct high heat.  A two inch thick t-bone should be cooked 14 to 18 minutes, with 6 to 8 minutes over direct high heat, and then 8 to 10 minutes over indirect high heat.",
+    "filet mignon":"a three quarter inch thick filet mignon, should be cooked for 4 to 6 minutes over direct high heat.  A one inch thick filet mignon, should be cooked 6 to 8 minutes over direct high heat.  A two inch thick filet mignon should be cooked 14 to 18 minutes, with 6 to 8 minutes over direct high heat, and then 8 to 10 minutes over indirect high heat.",
+    "beef tenerloin":"a beef tenderloin of 3 to 4 pounds, should be cooked for 45 to 60 minutes over indirect high heat.",
+    "flank steak":"a flank steak of 1 and a half to 2 pounds, and three quarters of an inch thick should be cooked for 8 to 10 minutes over direct medium heat",
+    "ground beef patty":"a hamburger that is three quarters of an inch thick, should be cooked for 8 to 10 minutes, over direct medium heat",
+    "hamburger":"a hamburger that is three quarters of an inch thick, should be cooked for 8 to 10 minutes, over direct medium heat",
+    "burger":"a hamburger that is three quarters of an inch thick, should be cooked for 8 to 10 minutes, over direct medium heat",
+    "bratwurst":"a bratwurst should be cooked for 20 to 25 minutes over direct low heat",
+    "brat":"a bratwurst should be cooked for 20 to 25 minutes over direct low heat",
+    "italian sausage":"a bratwurst should be cooked for 20 to 25 minutes over direct low heat",
+    "polish sausage":"a bratwurst should be cooked for 20 to 25 minutes over direct low heat",
+pork chop
+boneless pork chop
+bone in pork chop
+baby back ribs
+spareribs
+country style ribs
+pork tenerloin
+chicken pieces
+bone in chicken pieces
+bone in chicken
+bone in chicken thighs
+bone in chicken breasts
+bone in chicken legs
+chicken drum stick
+bone in chicken wings
+chicken wings
+whole chicken
+cornish game hen
+turkey
+whole turkey
+fish
+fish fillet 
+fish steak
+halibut
+halibut fillet
+halibut steak
+red snapper
+red snapper fillet
+red snapper steak
+salmon
+salmon fillet
+salmon steak
+sea bass
+sea bass fillet
+sea bass steak
+swordfish
+swordfish fillet
+swordfish steak
+tuna
+tuna fillet
+tuna steak
+shrimp
+asparagus
+corn
+mushrooms
+onion 
+potato
+    
+};
